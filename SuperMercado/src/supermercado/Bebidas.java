b76c5f415ac2078ba459a9b97e31e9066c6b7a8a
Luana-Cristina <luanacristina200@gmail.com>
2021-06-18 01:09:47 -0300

    agora vai

---
+++ SuperMercado/src/supermercado/Bebidas.java
@@ -0,0 +1,16 @@
+
+package supermercado;
+
+
+public class Bebidas extends Produtos {
+
+    public Bebidas(String nome, int quantidade, float preco) {
+        super(nome, quantidade, preco);
+    }
+    
+    @Override
+    public void formaPag() {
+        
+    }
+    
+}
---
+++ SuperMercado/src/supermercado/SuperMercado.java
@@ -0,0 +1,10 @@
+package supermercado;
+
+public class SuperMercado {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
